aa21a42a443bdad115d637dd07b42fd02a3d2465
Lijoen <linus.joensson.mail@gmail.com>
2016-03-04 13:46:20 +0100

    startup

--- src/ProjectCustomersMVC/Startup.cs
+++ src/ProjectCustomersMVC/Startup.cs
@@ -15,17 +15,15 @@ namespace ProjectCustomersMVC
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
